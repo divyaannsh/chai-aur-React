2e96bd410e6bdd009674b13b533fe4103fb377bb
Divyansh <divyanshsrivastav72@gmail.com>
2023-12-05 20:21:03 +0530

    v

--- NamasteReact/inception/ep01/ep01/ep02/app.js
+++ NamasteReact/inception/ep01/ep01/ep02/app.js
@@ -1,7 +1,11 @@
 import React from "react";
+ import  ReactDOM  from "react-dom/client";
 
-const heading = React.createElement("h2",{},"hello world From React");
+//  React ELement
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const heading = React.createElement("h1",{id: "heading"},"Namaste React");
+
+
+const root = React.createElement(document.getElementById("root"));
 
 root.render(heading)
